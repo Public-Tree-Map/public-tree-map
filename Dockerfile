b2638f6f5b12256d84d5391591b60fe79785c490
Chris Dower <chris.dower@icann.org>
2018-04-23 20:16:06 -0700

    create Migrations/Database

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,7 @@ RUN  pip3 install -r /opt/streetTreeMap/requirements.txt
 #  && python ./manage.py bower install 
 #like rake precomplie assets
 #Just until non-sqlite database is setup
-#RUN python /opt/django_project/manage.py migrate
+RUN python /opt/streetTreeMap/manage.py makemigrations \
+  && python /opt/streetTreeMap/manage.py migrate
 
 CMD python /opt/streetTreeMap/manage.py runserver
--- streetTreeMap/treeMap/models.py
+++ streetTreeMap/treeMap/models.py
@@ -14,20 +14,20 @@ class Species(models.Model):
   species = models.CharField(max_length=100, unique=True, verbose_name="Species")
 
 class Replacement_Species(models.Model):
-  species =  models.ForeignKey(Species, on_delete=models.SET_NULL)
-  replacements = models.ManyToManyField(Species)
+  species =  models.ForeignKey(Species, on_delete=models.CASCADE)
+  replacements = models.ManyToManyField(Species, related_name='replacements')
 
 class Tree_images(models.Model):
   image = models.FileField()
 
 class Tree_name(models.Model):
-  common_name = models.CharField(max_length=100,unique=True, verbose_name="Common Name")
+  common_name = models.CharField(max_length=255,unique=True, verbose_name="Common Name")
   native = models.BooleanField()
   invasive = models.BooleanField()
-  images = models.ForeignKey(Tree_images, on_delete=models.SET_NULL)
-  family = models.ForeignKey(Family,on_delete=models.SET_NULL)
-  genus = models.ForeignKey(Genus,on_delete=models.SET_NULL)
-  species = models.ForeignKey(Species,on_delete=models.SET_NULL)
+  images = models.ForeignKey(Tree_images, on_delete=models.SET_NULL, null=True)
+  family = models.ForeignKey(Family,on_delete=models.SET_NULL, null=True)
+  genus = models.ForeignKey(Genus,on_delete=models.SET_NULL, null=True)
+  species = models.ForeignKey(Species,on_delete=models.SET_NULL, null=True)
   #iucn status ????
 
 class Water_needs(models.Model):
@@ -39,16 +39,16 @@ class Prune_date(models.Model):
   last_prune = models.DateField()
 
 class Trees(models.Model):
-  common_name = models.ForeignKey(Tree_name, on_delete=models.SET_NULL)
+  common_name = models.ForeignKey(Tree_name, on_delete=models.SET_NULL, null=True)
   latitude = models.DecimalField(max_digits=12, decimal_places=9)
   longitude = models.DecimalField(max_digits=12, decimal_places=9)
   street_number = models.IntegerField()
   street_name = models.CharField(max_length=100)
-  stree_view_image = models.CharField(max_lenth=100)
+  stree_view_image = models.CharField(max_length=100)
   diameter = models.IntegerField()
   height = models.IntegerField()
   next_prune = models.DateField()
-  last_prune = models.ForeignKey(Prune_date, on_delete=models.SET_NULL)
+  last_prune = models.ForeignKey(Prune_date, on_delete=models.SET_NULL, null=True)
 
   def __str__(self):
     return self.common_name+"("+self.common_name+")"
