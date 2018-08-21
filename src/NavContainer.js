b4d54ad49a57bba32f9c6f7b38d72a9f0a7c3803
Jake <acosta.jacob.d@gmail.com>
2018-08-21 13:19:12 -0700

    removed bootstrap, moved css to fies, reproduced navContainer and Stats Container with Foundation

--- src/NavContainer.js
+++ src/NavContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Grid, GridItem } from 'styled-grid-component';
-import { Button } from 'react-bootstrap';
-
+import './NavContainer.css';
+import { Button, ButtonGroup } from 'react-foundation';
 
 
 export default class NavContainer extends Component {
@@ -12,92 +11,45 @@ export default class NavContainer extends Component {
 
     render() {
         return (
-            <Grid
-                style={styles.container}
-                width="30%"
-                templateColumns="repeat(9, 1fr)"
-                autoRows="minmax(5em)"
-                height="auto"
-            >
-                <GridItem column="1/10" row="1" style={{ padding: '1em' }}>
-                    Heatmap
-                    <div style={styles.buttonDiv}>
-                        <Button style={styles.button}>Canopy Cover</Button>
-                        <Button style={styles.button}>Height</Button>
-                    </div>
-                </GridItem>
-                <GridItem column="1/10" row="2" style={{ padding: '1em' }}>
-                    Filter
-                <div style={styles.buttonDiv}>
-                        <Button style={styles.button}>CA Native</Button>
-                        <Button style={styles.button}>Endangered</Button>
-                        <Button style={styles.button}>Heritage</Button>
-                        <Button style={styles.button}>Invasive</Button>
-                    </div>
-                </GridItem>
-                <GridItem column="1/10" row="3" style={{ width: '10em', paddingLeft: '1em'}}>Meet the Locals</GridItem>
-                <div>
-                    <Grid style={styles.locals}
-                        width="30%"
-                        templateColumns="repeat(9, 1fr)"
-                        autoRows="minmax(7em)"
-                    >
-                        <GridItem column="1/4" row="1" style={styles.localsButton}>One</GridItem>
-                        <GridItem column="4/7" row="1" style={styles.localsButton}>Two</GridItem>
-                        <GridItem column="7/10" row="1" style={styles.localsButton}>Three</GridItem>
-                        <GridItem column="1/4" row="2" style={styles.localsButton}>Four</GridItem>
-                        <GridItem column="4/7" row="2" style={styles.localsButton}>Five</GridItem>
-                        <GridItem column="7/10" row="2" style={styles.localsButton}>Six</GridItem>
-                        <GridItem column="1/4" row="3" style={styles.localsButton}>Seven</GridItem>
-                        <GridItem column="4/7" row="3" style={styles.localsButton}>Eight</GridItem>
-                        <GridItem column="7/10" row="3" style={styles.localsButton}>Nine</GridItem>
-                        <GridItem column="1/10" row="4">
-                            <form>
-                                <input type='text' name='searchNavBar' placeholder='Search all Species' style={styles.searchBar} />
-                            </form>
-                        </GridItem>
-                    </Grid>
+            <div className="container">
+                <div className="navGroup">
+                    <div>Heatmap</div>
+                    <ButtonGroup isExpanded className="buttonGroup">
+                        <Button className="button">Canopy Cover</Button>
+                        <Button className="button">Height</Button>
+                    </ButtonGroup>
                 </div>
-            </Grid>
+                <div className="navGroup">
+                    <div>Filter</div>
+                    <ButtonGroup isExpanded className="buttonGroup">
+                        <Button className="button">CA Native</Button>
+                        <Button className="button">Endangered</Button>
+                        <Button className="button">Heritage</Button>
+                        <Button className="button">Invasive</Button>
+                    </ButtonGroup>
+                </div>
+                <div className="navGroup">
+                    <div >Meet the Locals</div>
+                    <ButtonGroup isExpanded className="buttonGroup">
+                        <Button className="localsButton">One</Button>
+                        <Button className="localsButton">One</Button>
+                        <Button className="localsButton">One</Button>
+                    </ButtonGroup>
+                    <ButtonGroup isExpanded className="buttonGroup">
+                        <Button className="localsButton">One</Button>
+                        <Button className="localsButton">One</Button>
+                        <Button className="localsButton">One</Button>
+                    </ButtonGroup>
+                    <ButtonGroup isExpanded className="buttonGroup">
+                        <Button className="localsButton">One</Button>
+                        <Button className="localsButton">One</Button>
+                        <Button className="localsButton">One</Button>
+                    </ButtonGroup>
+                </div>
+                <form className="searchBar">
+                    <input type='text' name='searchNavBar' placeholder='Search all Species' className="bar" />
+                </form>
+            </div>
         )
-    }
-}
-const styles = {
-    container: {
-        backgroundColor: '#f0f0f0',
-        textAlign: 'left',
-        right: '0px',
-        position: 'fixed',
-    },
-    locals: {
-        backgroundColor: '#f0f0f0',
-        position: 'fixed',
-    },
-    button: {
-        fontSize: 9,
-        height: '3em',
-        margin: '.2em',
-        borderRadius: 3,
-        float: 'left',
-        backgroundColor: '#d9d9d9',
-        color: '#828184',
-    },
-    localsButton: {
-        margin: '.5em',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '8em',
-        color: '#828184',
-        borderRadius: 3,
-        borderWidth: '.1em',
-        borderColor: 'grey',
-        borderStyle: 'solid',
-        backgroundColor: '#d9d9d9',
-    },
-    searchBar: {
-        height: '2em',
-        width: '95%',
-        marginLeft: '1em'
     }
 }
--- src/statsContainer.js
+++ src/statsContainer.js
@@ -1,73 +1,80 @@
 import React, { Component } from 'react';
-import { Grid, GridItem } from 'styled-grid-component';
+import './StatsContainer.css'
+import { Row, Column } from 'react-foundation';
 
 export default class StatsContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            species: 'Birch',
+            species: 'CA SYCAMORE',
             speciesTotal: 324,
-            urbanForest: 'SM Urban Forest',
+            urbanForest: 'SM URBAN FOREST',
             perTotal: '1%',
-            family: 'Plane Trees',
+            family: 'PLANE TREES',
             familyTotal: 1145,
             treePerTotal: '4%',
+            treeCount: 14500,
         };
-        //review passed down props/state
     }
+
+    //first view is total trees in view
+    //% of total native
+    //# of species in view
+    //% of species native
+    //# of families in view
+    //onClick to specific tree will show above stats for tree type
 
     render() {
         return (
-            <Grid
-                style={styles.container}
-                templateColumns="repeat(4, 1fr)"
-                autoRows="minmax(5em)"
-            >
-                <GridItem column="1/2" style={styles.box}>
-                    <div style={styles.stat}>{this.state.speciesTotal}</div>
-                    <div style={styles.number}>{this.state.species}</div>
-                </GridItem>
-                <GridItem column="2/3" style={styles.box}>
-                    <div style={styles.stat}>{this.state.perTotal}</div>
-                    <div style={styles.number}>{this.state.urbanForest}</div></GridItem>
-                <GridItem column="3/4" style={styles.box}>
-                    <div style={styles.stat}>{this.state.familyTotal}</div>
-                    <div style={styles.number}>{this.state.family}</div>
-                </GridItem>
-                <GridItem column="4/5" style={styles.box}>
-                    <div style={styles.stat}>{this.state.familyTotal}</div>
-                    <div style={styles.number}>{this.state.urbanForest}</div></GridItem>
-            </Grid>
+            <Row className="StatContainer">
+                <Column className="box">
+                    <div className="stat">{this.state.treeCount}</div>
+                    <div className="statInfo">TOTAL TREES IN VIEW</div>
+                    <div></div>
+                </Column>
+                <Column className="box">
+                    <div className="stat">{this.state.speciesTotal}</div>
+                    <div className="statInfo">{this.state.species}</div>
+                    <div className="KPCOFGS">Species</div>
+                </Column>
+                <Column className="box">
+                    <div className="stat">{this.state.perTotal}</div>
+                    <div className="statInfo">% OF TOTAL NATIVE</div>
+                </Column>
+                <Column className="box">
+                    <div className="stat">{this.state.familyTotal}</div>
+                    <div className="statInfo">{this.state.family}</div>
+                    <div className="KPCOFGS">Family</div>
+                </Column>
+                <Column className="box">
+                    <div className="stat">{this.state.treePerTotal}</div>
+                    <div className="statInfo">{this.state.urbanForest}</div>
+                    <div></div>
+                </Column>
+            </Row>
         )
     }
 }
 
-const styles = {
-    container: {
-        bottom: '0px',
-        position: 'fixed',
-        height: '20%',
-        width: '70%',
-        backgroundColor: 'lightgrey',
-    },
-    box: {
-        marginTop: '3em',
-        display: 'flex',
-        // justifyContent: 'center',
-        flexFlow: 'row wrap',
-        color: '#828184',
-    },
-    stat: {
-        fontSize: 36,
-        // width: '100%',
-        paddingLeft: '30%',
-        paddingRight: '30%',
-        justifyContent: 'center',
-        textAlign: 'center',   
-    },
-    number: {
-        fontSize: 18,
-        paddingLeft: '30%',
-
-    }
-}
+// <Grid
+            //     className='container'
+            //     templateColumns="repeat(4, 1fr)"
+            //     autoRows="minmax(5em)"
+            // >
+            //     <GridItem column="1/2" className='box'>
+            //         <div className='stat'>{this.state.speciesTotal}</div>
+            //         <div className='number'>{this.state.species}</div>
+            //         <div>Tree Species</div>
+            //     </GridItem>
+            //     <GridItem column="2/3" className='box'>
+            //         <div className='stat'>{this.state.perTotal}</div>
+            //         <div className='number'>{this.state.urbanForest}</div></GridItem>
+            //     <GridItem column="3/4" className='box'>
+            //         <div className='stat'>{this.state.familyTotal}</div>
+            //         <div className='number'>{this.state.family}</div>
+            //         <div>Tree Family</div>
+            //     </GridItem>
+            //     <GridItem column="4/5" className='box'>
+            //         <div className='stat'>{this.state.treePerTotal}</div>
+            //         <div className='number'>{this.state.urbanForest}</div></GridItem>
+            // </Grid>
