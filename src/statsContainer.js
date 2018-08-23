1c62121db0f6734f48a2eb7a9e009672ee2cbf84
Jake <acosta.jacob.d@gmail.com>
2018-08-22 22:14:02 -0700

    moved css to sep files, added Foundation for React

--- src/statsContainer.js
+++ src/statsContainer.js
@@ -17,13 +17,6 @@ export default class StatsContainer extends Component {
         };
     }
 
-    //first view is total trees in view
-    //% of total native
-    //# of species in view
-    //% of species native
-    //# of families in view
-    //onClick to specific tree will show above stats for tree type
-
     render() {
         return (
             <Row className="StatContainer">
@@ -55,26 +48,3 @@ export default class StatsContainer extends Component {
         )
     }
 }
-
-// <Grid
-            //     className='container'
-            //     templateColumns="repeat(4, 1fr)"
-            //     autoRows="minmax(5em)"
-            // >
-            //     <GridItem column="1/2" className='box'>
-            //         <div className='stat'>{this.state.speciesTotal}</div>
-            //         <div className='number'>{this.state.species}</div>
-            //         <div>Tree Species</div>
-            //     </GridItem>
-            //     <GridItem column="2/3" className='box'>
-            //         <div className='stat'>{this.state.perTotal}</div>
-            //         <div className='number'>{this.state.urbanForest}</div></GridItem>
-            //     <GridItem column="3/4" className='box'>
-            //         <div className='stat'>{this.state.familyTotal}</div>
-            //         <div className='number'>{this.state.family}</div>
-            //         <div>Tree Family</div>
-            //     </GridItem>
-            //     <GridItem column="4/5" className='box'>
-            //         <div className='stat'>{this.state.treePerTotal}</div>
-            //         <div className='number'>{this.state.urbanForest}</div></GridItem>
-            // </Grid>
