f42b2a9786ede544d57f021336eef113e0f92b22
tpetersen0308 <travis.e.petersen@gmail.com>
2018-09-18 13:30:04 -0700

    add child div for location info

--- src/SingleTree.js
+++ src/SingleTree.js
@@ -8,6 +8,21 @@ export const SingleTree = props => {
         <p>{props.tree.speciesName}</p>
         <img src={props.tree.image} alt={props.tree.commonName} />
       </div>
+      <div className="locationInfo">
+        <h4>Location</h4>
+        <p>Nearest Address:</p>
+        <p>
+          {props.tree.location.nearestAddress.street}
+          <br />
+          {props.tree.location.nearestAddress.city}
+        </p>
+        <p>Tree ID:</p>
+        <p>{props.tree.location.treeId}</p>
+        <img
+          src={props.tree.location.streetViewImage}
+          alt="Street View Image"
+        />
+      </div>
     </div>
   );
 };
