39724b0722beaddae69795c6fbc71794008d2dce
tpetersen0308 <travis.e.petersen@gmail.com>
2018-09-18 13:15:14 -0700

    refactor to use props argument

--- src/SingleTree.js
+++ src/SingleTree.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export const SingleTree = tree => {
+export const SingleTree = props => {
   return (
     <div className="singleTreeView">
       <div className="basicInfo">
-        <h1>{tree.commonName}</h1>
-        <p>{tree.speciesName}</p>
-        <img src={tree.image} alt={tree.commonName} />
+        <h1>{props.tree.commonName}</h1>
+        <p>{props.tree.speciesName}</p>
+        <img src={props.tree.image} alt={props.tree.commonName} />
       </div>
     </div>
   );
