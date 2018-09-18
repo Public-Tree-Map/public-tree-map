29c9cdb07e1473ec0cc51766050e33f8ada536fb
tpetersen0308 <travis.e.petersen@gmail.com>
2018-09-18 12:42:39 -0700

    add parent singleTreeView div and child div with basic name info

--- src/SingleTree.js
+++ src/SingleTree.js
@@ -1,3 +1,12 @@
 import React from 'react';
 
-export const SingleTree = tree => {};
+export const SingleTree = tree => {
+  return (
+    <div className="singleTreeView">
+      <div className="basicInfo">
+        <h1>{tree.commonName}</h1>
+        <p>{tree.speciesName}</p>
+      </div>
+    </div>
+  );
+};
