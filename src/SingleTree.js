aceafdbd955ca98e0ebe9d7849c57a937038360a
tpetersen0308 <travis.e.petersen@gmail.com>
2018-09-18 17:00:09 -0700

    change p tags to divs and add css class to property details

--- src/SingleTree.js
+++ src/SingleTree.js
@@ -5,20 +5,20 @@ export const SingleTree = props => {
   return (
     <div className="singleTreeView">
       <div className="basicInfo">
-        <h1>{props.tree.commonName}</h1>
-        <p>{props.tree.speciesName}</p>
+        <h1 className="treeTitle">{props.tree.commonName}</h1>
+        <div className="properyDetails">{props.tree.speciesName}</div>
         <img src={props.tree.image} alt={props.tree.commonName} />
       </div>
       <div className="locationInfo">
         <h4>Location</h4>
-        <p className="propertyLabel">Nearest Address:</p>
-        <p>
+        <div className="propertyLabel">Nearest Address:</div>
+        <div className="properyDetails">
           {props.tree.location.nearestAddress.street}
           <br />
           {props.tree.location.nearestAddress.city}
-        </p>
-        <p className="propertyLabel">Tree ID:</p>
-        <p>{props.tree.location.treeId}</p>
+        </div>
+        <div className="propertyLabel">Tree ID:</div>
+        <div className="properyDetails">{props.tree.location.treeId}</div>
         <img
           src={props.tree.location.streetViewImage}
           alt="Street View Image"
@@ -26,50 +26,70 @@ export const SingleTree = props => {
       </div>
       <div className="sizeInfo">
         <h4>Size</h4>
-        <p className="propertyLabel">Height Range:</p>
-        <p>{props.tree.size.heightRange}</p>
-        <p className="propertyLabel">Trunk Diameter (DBH) Range:</p>
-        <p>{props.tree.size.trunkDiameterRange}</p>
-        <p className="propertyLabel">Species Height By Width:</p>
-        <p>{props.tree.size.speciesHeightByWidth}</p>
+        <div className="propertyLabel">Height Range:</div>
+        <div className="properyDetails">{props.tree.size.heightRange}</div>
+        <div className="propertyLabel">Trunk Diameter (DBH) Range:</div>
+        <div className="properyDetails">
+          {props.tree.size.trunkDiameterRange}
+        </div>
+        <div className="propertyLabel">Species Height By Width:</div>
+        <div className="properyDetails">
+          {props.tree.size.speciesHeightByWidth}
+        </div>
       </div>
       <div className="valueInfo">
         <h4>Tree Value</h4>
-        <p className="propertyLabel">Trees Required To Replace:</p>
-        <p>{props.tree.treeValue.treesToReplace}</p>
-        <p className="propertyLabel">Shade Production:</p>
-        <p>{props.tree.treeValue.shadeProduction}</p>
-        <p className="propertyLabel">Shedability:</p>
-        <p>{props.tree.treeValue.shedability}</p>
+        <div className="propertyLabel">Trees Required To Replace:</div>
+        <div className="properyDetails">
+          {props.tree.treeValue.treesToReplace}
+        </div>
+        <div className="propertyLabel">Shade Production:</div>
+        <div className="properyDetails">
+          {props.tree.treeValue.shadeProduction}
+        </div>
+        <div className="propertyLabel">Shedability:</div>
+        <div className="properyDetails">{props.tree.treeValue.shedability}</div>
       </div>
       <div className="activityInfo">
         <h4>Care {'\u0026'} Activity</h4>
-        <p className="propertyLabel">
+        <div className="propertyLabel">
           Recommended Watering (For Mature Trees):
-        </p>
-        <p>{props.tree.careAndActivity.recommendedWatering}</p>
-        <p className="propertyLabel">Next City Maintenance:</p>
-        <p>{props.tree.careAndActivity.nextCityMaintenance}</p>
-        <p className="propertyLabel">Management Unit:</p>
-        <p>{props.tree.careAndActivity.managementUnit}</p>
+        </div>
+        <div className="properyDetails">
+          {props.tree.careAndActivity.recommendedWatering}
+        </div>
+        <div className="propertyLabel">Next City Maintenance:</div>
+        <div className="properyDetails">
+          {props.tree.careAndActivity.nextCityMaintenance}
+        </div>
+        <div className="propertyLabel">Management Unit:</div>
+        <div className="properyDetails">
+          {props.tree.careAndActivity.managementUnit}
+        </div>
       </div>
       <div className="santaMonicaInfo">
         <h4>In Santa Monica</h4>
-        <p className="propertyLabel">This Species In Santa Monica:</p>
-        <p>{props.tree.inSantaMonica.thisSpecies}</p>
-        <p className="propertyLabel">Plane Family In Santa Monica:</p>
-        <p>{props.tree.inSantaMonica.planeFamily}</p>
+        <div className="propertyLabel">This Species In Santa Monica:</div>
+        <div className="properyDetails">
+          {props.tree.inSantaMonica.thisSpecies}
+        </div>
+        <div className="propertyLabel">Plane Family In Santa Monica:</div>
+        <div className="properyDetails">
+          {props.tree.inSantaMonica.planeFamily}
+        </div>
       </div>
       <div className="originInfo">
         <h4>Origin</h4>
-        <p>Origin:</p>
-        <p>{props.tree.origin.origin}</p>
-        <p className="propertyLabel">Native Distribution In California:</p>
-        <p>{props.tree.origin.nativeDistribution}</p>
-        <p className="propertyLabel">Natural Habitat In California</p>
-        <p>{props.tree.origin.naturalHabitat}</p>
-        <p className="propertyLabel">Fun Fact:</p>
-        <p>{props.tree.origin.funFact}</p>
+        <div className="propertyLabel">Origin:</div>
+        <div className="properyDetails">{props.tree.origin.origin}</div>
+        <div className="propertyLabel">Native Distribution In California:</div>
+        <div className="properyDetails">
+          {props.tree.origin.nativeDistribution}
+        </div>
+        <div className="propertyLabel">Natural Habitat In California</div>
+        <div className="properyDetails">{props.tree.origin.naturalHabitat}</div>
+        <div className="propertyLabel">Fun Fact:</div>
+        <div className="properyDetails">{props.tree.origin.funFact}</div>
       </div>
     </div>
   );
