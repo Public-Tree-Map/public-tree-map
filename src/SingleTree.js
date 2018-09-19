25d6815b14687766b90ae1cbdae7976977493432
tpetersen0308 <travis.e.petersen@gmail.com>
2018-09-18 17:04:37 -0700

    fix typo in propertyDetails className

--- src/SingleTree.js
+++ src/SingleTree.js
@@ -6,19 +6,19 @@ export const SingleTree = props => {
     <div className="singleTreeView">
       <div className="basicInfo">
         <h1 className="treeTitle">{props.tree.commonName}</h1>
-        <div className="properyDetails">{props.tree.speciesName}</div>
+        <div className="propertyDetails">{props.tree.speciesName}</div>
         <img src={props.tree.image} alt={props.tree.commonName} />
       </div>
       <div className="locationInfo">
         <h4>Location</h4>
         <div className="propertyLabel">Nearest Address:</div>
-        <div className="properyDetails">
+        <div className="propertyDetails">
           {props.tree.location.nearestAddress.street}
           <br />
           {props.tree.location.nearestAddress.city}
         </div>
         <div className="propertyLabel">Tree ID:</div>
-        <div className="properyDetails">{props.tree.location.treeId}</div>
+        <div className="propertyDetails">{props.tree.location.treeId}</div>
         <img
           src={props.tree.location.streetViewImage}
           alt="Street View Image"
@@ -27,69 +27,73 @@ export const SingleTree = props => {
       <div className="sizeInfo">
         <h4>Size</h4>
         <div className="propertyLabel">Height Range:</div>
-        <div className="properyDetails">{props.tree.size.heightRange}</div>
+        <div className="propertyDetails">{props.tree.size.heightRange}</div>
         <div className="propertyLabel">Trunk Diameter (DBH) Range:</div>
-        <div className="properyDetails">
+        <div className="propertyDetails">
           {props.tree.size.trunkDiameterRange}
         </div>
         <div className="propertyLabel">Species Height By Width:</div>
-        <div className="properyDetails">
+        <div className="properytDetails">
           {props.tree.size.speciesHeightByWidth}
         </div>
       </div>
       <div className="valueInfo">
         <h4>Tree Value</h4>
         <div className="propertyLabel">Trees Required To Replace:</div>
-        <div className="properyDetails">
+        <div className="propertyDetails">
           {props.tree.treeValue.treesToReplace}
         </div>
         <div className="propertyLabel">Shade Production:</div>
-        <div className="properyDetails">
+        <div className="propertyDetails">
           {props.tree.treeValue.shadeProduction}
         </div>
         <div className="propertyLabel">Shedability:</div>
-        <div className="properyDetails">{props.tree.treeValue.shedability}</div>
+        <div className="propertyDetails">
+          {props.tree.treeValue.shedability}
+        </div>
       </div>
       <div className="activityInfo">
         <h4>Care {'\u0026'} Activity</h4>
         <div className="propertyLabel">
           Recommended Watering (For Mature Trees):
         </div>
-        <div className="properyDetails">
+        <div className="propertyDetails">
           {props.tree.careAndActivity.recommendedWatering}
         </div>
         <div className="propertyLabel">Next City Maintenance:</div>
-        <div className="properyDetails">
+        <div className="propertyDetails">
           {props.tree.careAndActivity.nextCityMaintenance}
         </div>
         <div className="propertyLabel">Management Unit:</div>
-        <div className="properyDetails">
+        <div className="propertyDetails">
           {props.tree.careAndActivity.managementUnit}
         </div>
       </div>
       <div className="santaMonicaInfo">
         <h4>In Santa Monica</h4>
         <div className="propertyLabel">This Species In Santa Monica:</div>
-        <div className="properyDetails">
+        <div className="propertyDetails">
           {props.tree.inSantaMonica.thisSpecies}
         </div>
         <div className="propertyLabel">Plane Family In Santa Monica:</div>
-        <div className="properyDetails">
+        <div className="propertyDetails">
           {props.tree.inSantaMonica.planeFamily}
         </div>
       </div>
       <div className="originInfo">
         <h4>Origin</h4>
         <div className="propertyLabel">Origin:</div>
-        <div className="properyDetails">{props.tree.origin.origin}</div>
+        <div className="propertyDetails">{props.tree.origin.origin}</div>
         <div className="propertyLabel">Native Distribution In California:</div>
-        <div className="properyDetails">
+        <div className="propertyDetails">
           {props.tree.origin.nativeDistribution}
         </div>
         <div className="propertyLabel">Natural Habitat In California</div>
-        <div className="properyDetails">{props.tree.origin.naturalHabitat}</div>
+        <div className="propertyDetails">
+          {props.tree.origin.naturalHabitat}
+        </div>
         <div className="propertyLabel">Fun Fact:</div>
-        <div className="properyDetails">{props.tree.origin.funFact}</div>
+        <div className="propertyDetails">{props.tree.origin.funFact}</div>
       </div>
     </div>
   );
