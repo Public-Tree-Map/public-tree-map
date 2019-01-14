4aef3e8ba6d8eecf75cb62635c5e2b4b039ff923
Ian Rose <ian.r.rose@gmail.com>
2019-01-14 11:03:45 -0800

    Load JSON data from GFS.

---
+++ public/js/data.js
@@ -0,0 +1,9 @@
+var app = this.app || {};
+
+(function(module) {
+  const DATA_URL = "https://storage.googleapis.com/public-tree-map/js/trees.json";
+  fetch(DATA_URL).then(response => {
+    response.json().then(data => app.setData(data));
+  });
+}
+})(app);
