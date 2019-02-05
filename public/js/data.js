3a3f2f456848e12ad95091267e4192f9a83c77cb
Ian Rose <ian.r.rose@gmail.com>
2019-02-04 18:47:40 -0800

    Flatten anticipated static path.

--- public/js/data.js
+++ public/js/data.js
@@ -1,7 +1,7 @@
 var app = this.app || {};
 
 (function(module) {
-  const DATA_URL = "https://storage.googleapis.com/public-tree-map/js/trees.json";
+  const DATA_URL = "https://storage.googleapis.com/public-tree-map/trees.json";
   fetch(DATA_URL).then(response => {
     response.json().then(data => app.setData(data));
   });
