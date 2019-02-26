84ac66442e1ec0f2ca9b5008c5fc631d5f8b6b8b
Greyson Parrelli <greyson.parrelli@gmail.com>
2019-02-25 19:59:38 -0800

    Render newly added tree attributes.

--- public/js/Sidebar.js
+++ public/js/Sidebar.js
@@ -3,14 +3,20 @@ var app = this.app || {};
 (function(module) {
 
   function Sidebar() {
-    this.defaultScreen = document.getElementById('sidebar-default');
-    this.treeContainer = document.getElementById('sidebar-tree');
-    this.image         = document.getElementById('sidebar-image');
-    this.commonName    = document.getElementById('sidebar-common-name');
-    this.botanicalName = document.getElementById('sidebar-botanical-name');
-    this.nativity      = document.getElementById('sidebar-nativity');
-    this.heightGroup   = document.getElementById('sidebar-height-group');
-    this.closeButton   = document.getElementById('sidebar-close-button');
+    this.defaultScreen          = document.getElementById('sidebar-default');
+    this.treeContainer          = document.getElementById('sidebar-tree');
+    this.image                  = document.getElementById('sidebar-image');
+    this.commonName             = document.getElementById('sidebar-common-name');
+    this.botanicalName          = document.getElementById('sidebar-botanical-name');
+    this.nativity               = document.getElementById('sidebar-nativity');
+    this.heightGroup            = document.getElementById('sidebar-height-group');
+    this.shadeProduction        = document.getElementById('sidebar-shade-production');
+    this.irrigationRequirements = document.getElementById('sidebar-irrigation-requirements');
+    this.form                   = document.getElementById('sidebar-form');
+    this.type                   = document.getElementById('sidebar-type');
+    this.iucnStatus             = document.getElementById('sidebar-iucn-status');
+    this.ipcRating              = document.getElementById('sidebar-ipc-rating');
+    this.closeButton            = document.getElementById('sidebar-close-button');
   }
 
   Sidebar.prototype.setTree = function(tree) {
@@ -21,10 +27,16 @@ var app = this.app || {};
     this.treeContainer.classList.remove('hidden');
     this.defaultScreen.classList.add('hidden');
 
-    this.commonName.innerText    = tree.name_common;
-    this.botanicalName.innerText = tree.name_botanical;
-    this.nativity.innerText      = tree.nativity;
-    this.heightGroup.innerText   = tree.height_group;
+    this.commonName.innerText             = tree.name_common;
+    this.botanicalName.innerText          = tree.name_botanical;
+    this.nativity.innerText               = tree.nativity;
+    this.heightGroup.innerText            = tree.height_group;
+    this.shadeProduction.innerText        = tree.shade_production;
+    this.irrigationRequirements.innerText = tree.irrigation_requirements;
+    this.form.innerText                   = tree.form;
+    this.type.innerText                   = tree.type;
+    this.iucnStatus.innerText             = tree.iucn_status;
+    this.ipcRating.innerText              = tree.ipc_rating;
 
     if (tree.images && tree.images.length > 0) {
       this.image.style.backgroundImage = 'url(' + tree.images[0] + ')';
