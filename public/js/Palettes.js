0cfd5093cf9942d47767ada6b3cf9b6683d8f639
emillipede <emily@compiler.la>
2019-08-30 15:44:38 -0700

    Revert "add species palette"
    This reverts commit 5cf118a725f38cfe4afdb789c24a0f2f66cd6c5e.

--- public/js/Palettes.js
+++ public/js/Palettes.js
@@ -15,56 +15,10 @@ var app = this.app || {};
       'field'    : 'name_common'
     },
     'family_name_botanical': {
-      'generated': false,
+      'generated': true,
       'field'    : 'family_name_botanical',
-      'Adoxaceae': {'color': '#9D848E', 'title': ''},
-      'Altingiaceae': {'color': '#2EA785', 'title': ''},
-      'Anacardiaceae': {'color': '#A47149', 'title': ''},
-      //'Apocynaceae':
-      'Aquifoliaceae': {'color': '#0F9D76', 'title': ''},
-      //'Araliaceae':
-      'Araucariaceae': {'color': '#63A19C', 'title': ''},
-      'Arecaceae': { 'color': '#AFAF5E', 'title': '' },
-      'Asparagaceae': {'color': '#D2CDB4', 'title': ''},
-      'Betulaceae': {'color': '#DDD5C7', 'title': ''},
-      'Bignoniaceae': {'color': '#9B7DD4', 'title': ''},
-      //'Casuarinaceae':
-      'Celastraceae': {'color': '#D93744', 'title': ''},
-      'Cupressaceae': {'color': '#545A3E', 'title': ''},
-      //'Cyatheaceae':
-      //'Ebenaceae':
-      'Ericaceae': {'color': '#B7C0D6', 'title': ''},
-      'Fabaceae': {'color': '#82B185', 'title': ''},
-      'Fagaceae': {'color': '#12674A', 'title': ''},
-      //'Ginkgoaceae':
-      'Juglandaceae': {'color': '#776A5F', 'title': ''},
-      'Lauraceae': {'color': '#ADBBA1', 'title': ''}, //alt: avocado 676232//
-      //'Lythraceae':
-      //'Magnoliaceae': {'color': '#FDB462', 'title': ''},
-      'Malvaceae': {'color': '#DD3848', 'title': ''},
-      'Meliaceae': {'color': '#824D46', 'title': ''},
-      //'Menispermaceae':
-      'Musaceae': {'color': '#FCB953', 'title': ''},
-      'Moraceae': {'color': '#532D3B', 'title': ''},
-      'Myrtaceae': { 'color': '#4F6B58', 'title': ''},
-      'Oleaceae': {'color': '#73714E', 'title': ''},
-      'Pinaceae': { 'color': '#3A795E', 'title': ''},
-      //'Pittosporaceae':
-      'Platanaceae': { 'color': '#35463D', 'title': ''},
-      'Podocarpaceae': {'color': '#008C45', 'title': ''},
-      //'Proteaceae':
-      //'Quillajaceae':
-      'Rosaceae': {'color': '#813639', 'title': ''},
-      'Rutaceae': { 'color': '#F9AC2F', 'title': ''},
-      'Salicaceae': {'color': '#8E4483', 'title': ''},
-      'Sapindaceae': {'color': '#C9A38D', 'title': ''},
-      //'Scrophulariaceae':
-      //'Simaroubaceae':
-      'Strelitziaceae': {'color': '#FF8C55', 'title': ''},
-      'Ulmaceae': {'color': '#547053', 'title': ''},
-      //'Verbenaceae':
-      'default'  : '#ffffff'
-      //view palette: https://www.colorzilla.com/colors/9D848E+2EA785+A47149+0F9D76+63A19C+AFAF5E+D2CDB4+DDD5C7+9B7DD4+D93744+545A3E+B7C0D6+82B185+12674A+776A5F+ADBBA1+DD3848+824D46+FCB953+532D3B+4F6B58+73714E+3A795E+35463D+008C45+813639+F9AC2F+8E4483+C9A38D+FF8C55+547053+colors
+      'Arecaceae': { 'color': '#ff0000', 'title': '' },
+      'default'  : '#000000'
     },
     'irrigation_requirements': {
       'generated'             : false,
