50d8de3dd6a9c2235a20d02fb519733475af4943
Mai <24775373+mrtmrt233@users.noreply.github.com>
2020-05-17 14:38:39 -0700

    fix bounds

--- public/js/Map.js
+++ public/js/Map.js
@@ -28,7 +28,9 @@ var app = this.app || {};
       ]
     });
 
-    this.leafletMap.setMaxBounds(this.leafletMap.getBounds().pad(0.2));
+    // Rectangular bounds for locate button, hard code to fix mobile bug
+    const bounds = [[34.049242,-118.417049],[34.006524,-118.530877]]
+    this.leafletMap.setMaxBounds(bounds);
 
     this.locateButton = L.control.locate({
       position: 'bottomright',
